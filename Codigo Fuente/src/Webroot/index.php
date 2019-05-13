96693d05f609c358676d68f8c3374548b65d408b
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-05-13 14:04:48 -0300

    Agrego handler de excepciones

--- Codigo Fuente/src/Webroot/index.php
+++ Codigo Fuente/src/Webroot/index.php
@@ -8,18 +8,30 @@ require_once ROOT . 'router.php';
 require_once ROOT . 'request.php';
 require_once ROOT . 'dispatcher.php';
 
-function getBaseAddress() {
-    $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
-    $filenamepattern = '/'.basename(__FILE__).'/';
-    $url = $protocol.'://'.$_SERVER['HTTP_HOST'].preg_replace($filenamepattern,"",$_SERVER['REQUEST_URI']);
-    return substr_replace($url, "", strripos($url, "/src/") + 5);
-}
-
 function throwError404()
 {
     http_response_code(404);
     include ROOT . "Views/NoCompletado/noCompletado.php";
     die();
+}
+
+function globalExceptionHandler($exception)
+{
+    $strError = "Error " . $exception->getCode() . ":" . $exception->getMessage();
+    echo $strError;
+    $strLog = "[". date("Y-m-d H:i:s") ."]" . $strError . PHP_EOL;
+    file_put_contents("exception-log.txt", $strLog,FILE_APPEND);
+    throwError404();
+}
+
+set_exception_handler('globalExceptionHandler');
+
+function getBaseAddress()
+{
+    $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+    $filenamepattern = '/' . basename(__FILE__) . '/';
+    $url = $protocol . '://' . $_SERVER['HTTP_HOST'] . preg_replace($filenamepattern, "", $_SERVER['REQUEST_URI']);
+    return substr_replace($url, "", strripos($url, "/src/") + 5);
 }
 
 session_start();
